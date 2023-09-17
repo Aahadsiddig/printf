8473cb674d06bbd00e53f51975f17229f206e4f9
Aahadsiddig <aahadsiddigo@gmail.com>
2023-09-16 18:55:38 -0700

    done

--- 0_printf.c
+++ 0_printf.c
@@ -44,7 +44,7 @@ int _printf(const char *format, ...)
 			printed_chars += printed;
 		}
 	}
-	
+
 	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
@@ -62,7 +62,7 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
-	
+
 	*buff_ind = 0;
 }
 
