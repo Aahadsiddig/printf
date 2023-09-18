92e1d046dca64b7624f15756399363217ab4423e
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-09-18 12:12:27 -0700

    marwa done

--- print_unsignedn_uhex.c
+++ print_unsignedn_uhex.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_hexa_upper - a function that Prints an unsigned number
+ *	in upper hexa notation
+ * @types: a List of arguments in the function
+ * @buffer: a buffer array in the fuction
+ * @flags: it Calculates active flags of the function
+ * @width: finds width of the function
+ * @precision: gets a Precision
+ * @size: get a Size in the func
+ * Return: a number of chars that will be printed
 */
 
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	return (print_hexa(types, "0123456789ABCDEF", buffer,
-		flags, 'X', width, precision, size));
+	return (print_hexa(types, "0123456789ABCDEF", buffer, flags,
+				'X', width, precision, size));
 }
