30c070ec685ef3e9d0ea39f0696d9dc564c52fad
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-09-18 11:33:49 -0700

    marwa done

--- print_int.c
+++ print_int.c
@@ -1,46 +1,46 @@
 #include "main.h"
 
 /**
- * print_int - Print int
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_int - a function that prints integer
+ * @types: a List of arguments of function
+ * @buffer: a buffer array
+ * @flags: it Calculates active flags of the func
+ * @width: gets a width of the function
+ * @precision: finds a precision
+ * @size: finds Size of the func
+ * Return: a number of charactors that will be printed
 */
 
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i = BUFF_SIZE - 2;
-	int is_negative = 0;
-	long int n = va_arg(types, long int);
-	unsigned long int num;
+	int x = BUFF_SIZE - 2;
+	int negative = 0;
+	long int z = va_arg(types, long int);
+	unsigned long int un;
 
-	n = convert_size_number(n, size);
+	z = convert_size_number(z, size);
 
-	if (n == 0)
-		buffer[i--] = '0';
+	if (z == 0)
+		buffer[x--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
-	num = (unsigned long int)n;
+	un = (unsigned long int)z;
 
-	if (n < 0)
+	if (z < 0)
 	{
-		num = (unsigned long int)((-1) * n);
-		is_negative = 1;
+		un = (unsigned long int)((-1) * z);
+		negative = 1;
 	}
 
-	while (num > 0)
+	while (un > 0)
 	{
-		buffer[i--] = (num % 10) + '0';
-		num /= 10;
+		buffer[x--] = (un % 10) + '0';
+		un /= 10;
 	}
 
-	i++;
+	x++;
 
-	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+	return (write_number(negative, x, buffer, flags, width, precision, size));
 }
 
